29f3cf4b3e63e2e0254a73937caf1e4961a04986
SAeeD508 <saeed.boroomand508@gmail.com>
2018-11-08 13:56:07 +0330

    Show More Animation

--- app/src/main/java/com/example/saeed/apptest/MainActivity.java
+++ app/src/main/java/com/example/saeed/apptest/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.saeed.apptest;
 
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+
+    @BindView(R.id.btn_showmore)
+    Button button;
+
+    @BindView(R.id.tvSongDes)
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +25,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        // button.setOnClickListener(new btnclicklistener());
+
+        button.setOnClickListener(this);
+
     }
 
 
+    @Override
+    public void onClick(View view) {
+
+
+      /*  if (button.getText().toString().equalsIgnoreCase("Showmore...")) {
+            textView.setMaxLines(Integer.MAX_VALUE);//your TextView
+            button.setText("Showless");
+        } else {
+            textView.setMaxLines(3);//your TextView
+            button.setText("Showmore...");
+        }*/
+   if (button.getText().toString().equalsIgnoreCase("Showmore...")) {
+       ObjectAnimator animation = ObjectAnimator.ofInt(textView, "maxLines", 40);
+       animation.setDuration(1000).start();
+            button.setText("Showless");
+        } else {
+       ObjectAnimator animation = ObjectAnimator.ofInt(textView, "maxLines", 3);
+       animation.setDuration(500).start();
+            button.setText("Showmore...");
+        }
+
+    }
+
+
+    private class btnclicklistener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+
+
+        }
+    }
 }
